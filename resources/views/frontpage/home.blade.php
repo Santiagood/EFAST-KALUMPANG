3e82cfe03d42147fe5f6e4e5a4ad0fbb7437d753
Santiagood <rainrainsantiago@gmail.com>
2022-09-02 09:57:37 +0800

    Fixes needed ni adam

--- resources/views/frontpage/home.blade.php
+++ resources/views/frontpage/home.blade.php
@@ -82,7 +82,7 @@
         </div>
       </div>
       <button class="flex px-8 py-2 mx-auto mt-16 text-lg text-white bg-indigo-500 border-0 rounded focus:outline-none hover:bg-indigo-600 ">
-        <a href="{{ route('dashboard') }}">
+        <a href="{{ route('register') }}">
             Get Started
         </a>
     </button>
